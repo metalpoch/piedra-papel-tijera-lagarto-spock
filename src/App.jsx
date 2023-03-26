b805125fdd618927cd6ab4c1161f592068aaa0a8
Keiber Urbila <keiberup.dev@gmail.com>
2023-03-26 15:58:10 -0400

    style: colors changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+import style from "./styles/App.module.css";
+//import Swal from "sweetalert2";
+//import withReactContent from "sweetalert2-react-content";
+import Navbar from "./components/Navbar";
+import Score from "./components/Score";
 
-import "./Index.css";
 import rules from "./assets/rules.png";
 import useFetch from "./hooks/useFetch";
 
@@ -15,12 +17,12 @@ const HANDS = {
   "🖖": "spock",
 };
 
-const MySwal = withReactContent(Swal);
+//const MySwal = withReactContent(Swal);
 
 function App() {
   const [url, setUrl] = useState(null);
   const [scorePc, setScorePc] = useState(0);
-  const [scoreUser, setScoreUser] = useState(0);
+  const [scorePlayer, setScorePlayer] = useState(0);
 
   const { data, loading, error } = useFetch(url);
 
@@ -28,7 +30,7 @@ function App() {
     if (data) {
       const { winner, msg } = data;
       if (winner === "PC") setScorePc(scorePc + 1);
-      if (winner === "Jugador") setScoreUser(scoreUser + 1);
+      if (winner === "Jugador") setScorePlayer(scorePlayer + 1);
 
       MySwal.fire({
         text: msg,
@@ -43,29 +45,44 @@ function App() {
   }, [data]);
 
   return (
-    <div className="App p-2" style={{ minHeight: "100vh" }}>
-      <div className="row justify-content-around">
-        <div className="col-3 col-md-1">
-          <h5>👤: {scoreUser}</h5>
-          <h5>🤖: {scorePc}</h5>
-          <div className="d-grid gap-1">
-            {Object.keys(HANDS).map((item) => (
-              <button
-                key={HANDS[item]}
-                className="btn btn-lg btn-primary"
-                onClick={() => setUrl(URL_BASE + HANDS[item])}
-              >
-                {item}
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="col-9 col-md-4">
-          <img src={rules} width="100%" />
-        </div>
-      </div>
-    </div>
+    <>
+      <header className={style.navbar}>
+        <Navbar />
+      </header>
+      <main className="bg-pink v-100">
+        <section className={style.score}>
+          <Score pc={scorePc} player={scorePlayer} />
+        </section>
+      </main>
+    </>
   );
+  {
+    /*
+     *  return (
+     *    <div className="App p-2" style={{ minHeight: "100vh" }}>
+     *      <div className="row justify-content-around">
+     *        <div className="col-3 col-md-1">
+     *          <h5>👤: {scoreUser}</h5>
+     *          <h5>🤖: {scorePc}</h5>
+     *          <div className="d-grid gap-1">
+     *            {Object.keys(HANDS).map((item) => (
+     *              <button
+     *                key={HANDS[item]}
+     *                className="btn btn-lg btn-primary"
+     *                onClick={() => setUrl(URL_BASE + HANDS[item])}
+     *              >
+     *                {item}
+     *              </button>
+     *            ))}
+     *          </div>
+     *        </div>
+     *        <div className="col-9 col-md-4">
+     *          <img src={rules} width="100%" />
+     *        </div>
+     *      </div>
+     *    </div>
+     *  );*/
+  }
 }
 
 export default App;
